28b498684e574cd3c0834918debf2949b068f0cf
Voroshylo <152655013+Voroshylo@users.noreply.github.com>
2024-05-26 14:08:04 +0300

    1

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,8 +1,9 @@
+
 const API_KEY = '44071791-e24b31a34b7a75e1ae02e9c2e';
-const BASE_URL = 'http://pixabay.com/api/';
+const BASE_URL = 'https://pixabay.com/api/';
 
 export async function fetchImages(query) {
-  const response = await fetch('${BASE_URL}?key=${API_KEY}&q=${encodeURIComponent(query)}&image_type=photo&orientation=horizontal&safesearch=true');
+  const response = await fetch(`${BASE_URL}?key=${API_KEY}&q=${encodeURIComponent(query)}&image_type=photo&orientation=horizontal&safesearch=true`);
   if (!response.ok) {
     throw new Error('Failed to fetch images');
   }
